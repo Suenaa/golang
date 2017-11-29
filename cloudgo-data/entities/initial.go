de2260baddea95c38946423c5caf8eeaa7c031cc
Suenaa <1165153863@qq.com>
2017-11-30 00:34:52 +0800

    commit cloudgo-data

---
+++ cloudgo-data/entities/initial.go
@@ -0,0 +1,23 @@
+package entities
+
+import(
+	"github.com/go-xorm/xorm"
+	"github.com/go-sql-driver/mysql"
+)
+
+var engine *xorm.Engine
+
+func init() {
+	e, err := xorm.NewEngine("mysql", "root:root@tcp(127.0.0.1:3305)/test?charset=utf8&parseTime=true")
+	if err != nil {
+		panic(err)
+	}
+	engine = e
+	engine.Sync2(new(UserInfo))
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
